d5165c8810a40c120a264eb0ad09b56c14954aea
志添 顏 <yan_manager@daprocn.com>
2026-08-10 04:19:13 +0800

    fix: allow designated staff sales reporting

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /* ZDOS PWA Service Worker｜Web Push + notificationclick deep link */
 /* eslint-disable no-restricted-globals */
 
-const SW_VERSION = 'zdos-sw-20260809-inventory-perm-002a';
+const SW_VERSION = 'zdos-sw-20260810-sales-report-perm-002b';
 const PRODUCTION_ORIGIN = 'https://zdos.app';
 
 function resolveSwOrigin() {
